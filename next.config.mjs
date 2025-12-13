3db5155e1bebe2701dedb99ec53476198bb75a49
ymjo140 <ymjo14@naver.com>
2025-12-13 19:26:33 +0900

    feat: add deployment config files

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://openapi.map.naver.com https://oapi.map.naver.com https://nrbe.pstatic.net https://*.vercel-scripts.com;",
               "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;", 
               "img-src 'self' data: blob: https://*.pstatic.net https://*.naver.com https://images.unsplash.com;",
-              "connect-src 'self' https://kr-col-ext.nelo.navercorp.com https://*.naver.com https://wemeet-backend-xqlo.onrender.com;", 
+              "connect-src 'self' https://kr-col-ext.nelo.navercorp.com https://*.naver.com https://wemeet-backend-xqlo.onrender.com wss://wemeet-backend-xqlo.onrender.com;", 
               "font-src 'self' data: https://cdn.jsdelivr.net;",
               "object-src 'none';",
               "base-uri 'self';",
